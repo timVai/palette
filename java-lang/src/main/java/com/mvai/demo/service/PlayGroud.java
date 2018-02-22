24527ad4642a8dc6a0487c061c5054bdf256c80a
fanfengshi <fanfengshi@jd.com>
2018-02-22 14:16:35 +0800

    a

--- java-lang/src/main/java/com/mvai/demo/service/PlayGroud.java
+++ java-lang/src/main/java/com/mvai/demo/service/PlayGroud.java
@@ -1,5 +1,6 @@
 package com.mvai.demo.service;
 
+import java.lang.reflect.Proxy;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -8,14 +9,26 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by fanfengshi on 2017/12/29.
  */
-public class PlayGroud {
+public class PlayGroud implements PlayGround{
 
 
-    private ExecutorService executorService = new ThreadPoolExecutor(5,
-            500,
-            1,
-            TimeUnit.MINUTES,
-            null,
-            new ThreadPoolExecutor.AbortPolicy());
+//    private ExecutorService executorService = new ThreadPoolExecutor(5,
+//            500,
+//            1,
+//            TimeUnit.MINUTES,
+//            null,
+//            new ThreadPoolExecutor.AbortPolicy());
+
+
+
+    public static void test(){
+        System.out.print("PlayGroud.test()");
+    }
+
+    @Override
+    public void test2(){
+        System.out.print("PlayGroud.test2()");
+    }
+
 
 }
